e207d7fe817052fa8bf0d2341a48151d368fbcba
Alexandre Ledru <alexandre.ledru@viacesi.fr>
2020-03-10 13:57:06 +0100

    massive design update

--- src/bundles/common/components/Home/Home.jsx
+++ src/bundles/common/components/Home/Home.jsx
@@ -4,14 +4,10 @@ import './Home.scss';
 class Home extends React.Component {
     render() {
         return (
-            <main>
-                <div className="row justify-content-center home">
-                    <div className="col-lg-12 col-sm-12 col-xs-12 main">
-                        <h1 className="text-center title1">Welcome to</h1>
-                        <h1 className="text-center title-main"> Ionia</h1>
-                    </div>
+            <div className="row justify-content-center home">
+                <div className="col-lg-12 col-sm-12 col-xs-12">
                 </div>
-            </main>
+            </div>
         )
     }
 }
